6394629d9f95677fc35dd0a37173f856f0424c0d
Bedrock02 <JimSteve91@gmal.com>
2013-11-04 22:41:36 -0500

    Link Model Changed

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,21 @@
 class Link < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+  
+  def updateScore
+  	self.score += 1
+  	self.save
+  end
+
+  def addComment(com)
+  	self.comments << com
+  	self.save
+  	self.score += com.score
+  	self.save
+  end
+
+  def seeScore
+  	puts self.score
+  end
+
 end
