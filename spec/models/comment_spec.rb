195b047e8bf37a99b8967466b090bf9b18a5bf3d
Bedrock02 <JimSteve91@gmal.com>
2013-11-04 22:39:57 -0500

    Comment Spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,12 +2,17 @@ require 'spec_helper'
 
 describe Comment do
   before(:each) do
-  	@comment = FactoryGirl.create(:comment)
+    @link = FactoryGirl.create(:link_with_comment)
+    @comment = @link.comments.first
   end
 
-  it "has a score"
+  it "has a score" do
+    @comment.score.should be_present
+  end
 
-  it "belongs to a link"
+  it "belongs to a link" do
+    @comment.link.should == @link
+  end
 
   it "increments the score" do
     ## this simulates the vote-up functionality.
