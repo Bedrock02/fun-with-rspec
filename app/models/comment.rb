91b92508ad40f0efc562e55a03cacb7d2ad756bc
Bedrock02 <JimSteve91@gmal.com>
2013-11-04 22:41:10 -0500

    Comment Model Changed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,26 @@
 class Comment < ActiveRecord::Base
   belongs_to :link
+ 	
+ 	after_save :updateParent
+  after_update :updateParent
+
+ 	def vote_up
+ 		self.score += 1
+ 		self.save
+  end
+
+  def updateParent
+  	
+  	if self.parent.nil?
+  		return
+  	else
+  		self.parent.updateScore
+  	end
+  
+  end
+ 	
+ 	def parent
+ 		return self.link
+ 	end
+
 end
